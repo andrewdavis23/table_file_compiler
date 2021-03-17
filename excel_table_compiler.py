fff13313749d7754c84615b8e958c5e6f40e613d
andrewdavis23 <47924318+andrewdavis23@users.noreply.github.com>
2021-03-16 20:25:43 -0400

    Add files via upload
    relabel to display file list.  todo:  save window, SQL query, checks and balances (similar tables, all CSV <- can i mix excel and CSV???), is the excel bug fixed yet?

--- excel_table_compiler.py
+++ excel_table_compiler.py
@@ -1,45 +1,88 @@
 from tkinter import *
 from tkinter import filedialog
 import pandas as pd
+import glob
+from time import sleep
+from progress.bar import Bar
 
-load_flag = False
-out_flag = False
+# pancake emoji
+e = chr(129374)
 
+# compilation
 all_data = pd.DataFrame()
 
 # select files and create display for window
 def open_file():
+    global files
+
     files = filedialog.askopenfilenames()
 
-    load_flag = True
-
-    print('files loaded:')
+    print(e+' files loaded:')
     for i in files:
         print(i)
 
-def out_dir():
-    directory = filedialog.askdirectory()
-    print('\noutput directory:\n'+directory)
+    # for label which shows user a list of files
+    files_list = '\n'
+    for i in files:
+        files_list += i + '\n'
 
-def compile(files):
-    pass
+    lbl_files['text'] = files_list  
+
+    # activate compile button
+    check_ready()
+
+def out_dir():
+    global directory
+    global out_flag
+
+    directory = filedialog.askdirectory()
+    print('{} output directory = {}'.format(e,directory))
+
+    out_flag = True
+
+    check_ready()
+
+def compile():
+    global files
+    global directory
+
+    print(e+' compiling...')
+    global all_data
+
+    for f in files:
+        temp_df = pd.read_csv(f)
+        # temp_df = temp_df.loc[temp_df.STORE_NUM==619]
+        all_data = all_data.append(temp_df, ignore_index=True)
+    
+    out_path = str(directory) + r'/output.csv'
+    print('{} out_path = {}'.format(e, out_path))
+    print('{} all_data.head() = \n{}'.format(e, all_data.head()))
+    all_data.to_csv(out_path,index=False)
+
+def check_ready():
+    global files
+
+    if len(files) > 0:
+        btn_compile["state"] = NORMAL
 
 window = Tk()
 window.title("CSV Compiler")
-window.geometry('300x300')
+window.geometry('225x250')
 
-lbl_msg = Label(window, text="Select the files that you wish to compile.\nAll files should contain the same headers.")
-lbl_sts = Label(window, text="files loaded {}\noutput folder {}".format(load_flag,out_flag))
-
-btn_open_file = Button(window, text="Select Files", command=open_file)
-btn_out_dir = Button(window, text="Select Output Folder", command=out_dir)
-btn_compile = Button(window, text="COMPILE", comman=compile)
-
+lbl_msg = Label(window, text="\nSelect the files and output folder.\nAll files should contain the same headers.\n")
 lbl_msg.grid(column=0, row=0)
 
+btn_open_file = Button(window, text="Select Files", command=open_file)
 btn_open_file.grid(column=0, row=1)
+
+btn_out_dir = Button(window, text="Select Output Folder", command=out_dir)
 btn_out_dir.grid(column=0, row=2)
 
-lbl_sts.grid(column=0, row=3)
+btn_compile = Button(window, text="COMPILE",state=DISABLED, command=compile)
+btn_compile.grid(column=0, row=3)
+
+lbl_files = Label(window, text='')
+lbl_files.grid(column=0, row=4)
 
 window.mainloop()
+
